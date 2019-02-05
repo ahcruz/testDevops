68d6b8c33be547ce870138ceb082d8491a12798e
Angel Horacio Cruz <angelhcruz@outlook.com>
2019-02-04 20:37:45 -0500

    Inicio

---
+++ DemoDevops/DemoDevops/DemoDevops/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DemoDevops
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
